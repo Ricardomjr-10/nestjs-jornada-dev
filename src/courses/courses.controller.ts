eb730ce545b2379f3e5a4e29a76f36416873f22a
Ricardo-10 <ricardomjr2@gmail.com>
2025-02-05 22:12:11 -0300

    criado metodo findAll em cousers.controller

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,9 @@
 import { Controller } from '@nestjs/common';
 
 @Controller('courses')
-export class CoursesController {}
+export class CoursesController {
+    // metodo findAll
+    findAll() {
+        return 'Listagem de cursos'
+    }
+}
