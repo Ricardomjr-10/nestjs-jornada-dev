ae06057b4868c12386bc3afc9751315e49435e86
Ricardo-10 <ricardomjr2@gmail.com>
2025-02-05 22:15:41 -0300

    criado metodo rota get para findAll

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,9 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
-    // metodo findAll
-    findAll() {
-        return 'Listagem de cursos'
-    }
+  @Get() // decorator do controller
+  // metodo findAll
+  findAll() {
+    return 'Listagem de cursos';
+  }
 }
