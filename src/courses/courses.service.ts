65c01d7ef7f7bf6077f3a3733d110fbbe9755af2
Ricardo-10 <ricardomjr2@gmail.com>
2025-02-07 22:07:20 -0300

    criando rotas

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -3,5 +3,41 @@ import { Course } from './intities/couse.intity';
 
 @Injectable() //provider
 export class CourseService {
-  private courses: Course = [];
+  private courses: Course[] = [
+    {
+      id: 1,
+      name: 'Fundamentos do framework NestJS',
+      descripition: 'Aprenda os fundamentos do framework NestJS',
+      tags: ['NestJS', 'Back-end', 'TypeScript', 'JavaScript'],
+    },
+  ];
+
+  findAll() {
+    return this.courses;
+  }
+
+  findOne(id: string) {
+    return this.courses.find((course: Course) => course.id === Number(id));
+  }
+
+  create(createCourseDto: any) {
+    this.courses.push(createCourseDto);
+  }
+
+  update(id: string, updateCourseDto: any) {
+    const indexCourse = this.courses.findIndex(
+      (course: Course) => course.id === Number(id),
+    );
+
+    this.courses[indexCourse] = updateCourseDto;
+  }
+
+  remove(id: string) {
+    const indexCourse = this.courses.findIndex(
+      (course: Course) => course.id === Number(id),
+    );
+    if (indexCourse >= 0) {
+      this.courses.splice(indexCourse, 1);
+    }
+  }
 }
